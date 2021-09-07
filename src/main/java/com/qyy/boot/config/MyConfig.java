fb9d074dfd60a5dff88fa6117237028ec3b7b7d3
qyy <tongyang0109@163.com>
2021-09-07 22:50:41 +0800

    @EnableConfigurationProperties(Car.class) 开启配置绑定

--- src/main/java/com/qyy/boot/config/MyConfig.java
+++ src/main/java/com/qyy/boot/config/MyConfig.java
@@ -1,7 +1,10 @@
 package com.qyy.boot.config;
 
+import com.qyy.boot.bean.Car;
 import com.qyy.boot.bean.Pet;
 import com.qyy.boot.bean.User;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
  * @Created by qyy
  */
 @Configuration   //告诉springBoot 容器,这是一个配置类 == 配置文件
+@EnableConfigurationProperties(Car.class)  //开启Car配置绑定功能  2 ,把这个Car自动注册到容器中
 public class MyConfig {
 
     @Bean  //给容器添加组件, 以方法名作为组件的id 返回类型就是组件的类型 , 返回值就是在容器中的实例
